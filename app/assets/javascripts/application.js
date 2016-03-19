35110e27a3822d6767045e407062e1493791ea1f
Andrew Woo <ahjwoo@gmail.com>
2016-03-19 01:22:54 -0700

    Added logo, changed styling, added flickr API and slick carousel implementation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,46 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+
+  $('#button').on('click', function(event){
+    event.preventDefault();
+
+  $('.image-slider').empty();
+
+    
+    var input = $('#inputarea').val();
+    var api_key = '906d24753a8a0da2e4e65975271d61cc';
+    var link = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + api_key + "&tags=" + input + "&format=json";
+
+    $.ajax({  
+      url: link,
+      jsonp: 'jsoncallback',
+      dataType: 'jsonp',
+      success: function(data) {
+        // console.log(data);
+        
+        for (var i = 0; i < 10; i++) {
+        var id = data.photos.photo[i].id;
+        var secret = data.photos.photo[i].secret;
+        var server = data.photos.photo[i].server;
+        var farm = data.photos.photo[i].farm;
+
+        var imageUrl = "http://farm" + farm + ".static.flickr.com/" + server + "/" + id + "_" + secret + ".jpg";
+
+        $('.image-slider').append('<img src=' + imageUrl + '>');
+        }
+
+        //Slick, an easy to integrate carousel.
+        $('.image-slider').slick({
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 1500,
+        });
+      }
+    });
+  });
+});
+
